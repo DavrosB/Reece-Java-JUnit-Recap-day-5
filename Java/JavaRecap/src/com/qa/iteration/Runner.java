ab58a11c2bd9da57f2c0ee0b91b5944952627af7
reece-elder <reece.elder@qa.com>
2021-08-03 16:11:50 +0100

    flowchart loops

--- Java/JavaRecap/src/com/qa/iteration/Runner.java
+++ Java/JavaRecap/src/com/qa/iteration/Runner.java
@@ -4,9 +4,12 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		System.out.println("Main working");
-		Demo.forLoop();
-		Demo.whileLoop();
-		Demo.doWhileLoop();
+//		Demo.forLoop();
+//		Demo.whileLoop();
+//		Demo.doWhileLoop();
+//		FlowChart.flowChart();
+//		FlowChart.flowChartTwo();
+		FlowChart.countTen();
 	}
 
 }
