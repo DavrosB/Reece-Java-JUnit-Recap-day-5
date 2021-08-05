e572bd6e3a4dc0c4aaa59bc6872dae5fd707c35d
reece-elder <reece.elder@qa.com>
2021-08-05 12:32:12 +0100

    JUnit fundamentals

---
+++ Java/TestingDemo/src/test/java/com/qa/demo/TestingDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.qa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// Annotation - You can annotate classes to tell Spring 
+// To do a specific thing
+@SpringBootTest
+class TestingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
